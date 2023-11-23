690386f31818b79e8b08fc852ff1855cb223ff7f
vasilecarp <vasilecarpconsulting@gmail.com>
2023-11-23 19:54:38 +0200

    Add react-currency-format package and Subtotal component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import './Checkout.css'
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -12,10 +13,18 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
         </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
       </div>
     </div>
   );
 }
 
-export default Checkout
+export default Checkout;
